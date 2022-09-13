2eac7969d99f7918991ba0bc284d438b7f1f5abb
Aleksandra Usacheva <lazercore@me.com>
2022-09-13 19:04:26 +0200

    Add current version

---
+++ script.js
@@ -0,0 +1,21 @@
+const taglineElement = document.querySelector('.hero__tagline')
+const titleElement = document.querySelector('.hero__title')
+const subtitleElement = document.querySelector('.hero__subtitle')
+
+// const settings = location.search.slice(1,location.search.length).split('&').map(i => i.split('=')).reduce((acc, cur) => Object.assign(acc, {[cur[0]]: decodeURI(cur[1])}), {})
+
+// for (key in settings) {
+// 	if (key === 'tags') {
+// 		taglineElement.innerHTML = '#' + settings[key].split(',').join(' #')
+// 	}
+// 	if (key === 'title') {
+// 		titleElement.innerHTML = settings[key]
+// 	}
+// 	if (key === 'date') {
+// 		subtitleElement.innerHTML = settings[key]
+// 	}
+// }
+
+const date = new Date()
+
+subtitleElement.innerHTML = `${date.getDate()}.${JSON.stringify(date.getMonth()).length === 1 ? '0' + date.getMonth() : date.getMonth()}.${date.getUTCFullYear()}`
